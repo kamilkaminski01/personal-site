3dfa899f0785b7f7f7b062351cb518b12026a2bf
Kamil Kamiński <kamilkaminski39@gmail.com>
2024-01-29 00:47:01 +0100

    Add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx'
 import './index.scss'
 import { BrowserRouter } from 'react-router-dom'
 import ScrollToTop from 'components/atoms/ScrollToTop'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ScrollToTop>
+        <Analytics />
         <App />
       </ScrollToTop>
     </BrowserRouter>
