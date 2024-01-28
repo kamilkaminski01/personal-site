f9a420e5247e8c31a229a469537ae59e1e563750
Kamil Kamiński <kamilkaminski39@gmail.com>
2024-01-28 23:48:46 +0100

    Remove BrowserRouter and create own router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import './App.scss'
 import HomePage from 'pages/HomePage'
-import { Route, Routes } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { PATHS } from 'utils/consts.ts'
 import ArchivePage from 'pages/ArchivePage'
+
+const router = createBrowserRouter([
+  {
+    path: PATHS.home,
+    element: <HomePage />
+  },
+  {
+    path: PATHS.archive,
+    element: <ArchivePage />
+  }
+])
 
 function App() {
   return (
     <div className="app">
-      <Routes>
-        <Route path={PATHS.home} element={<HomePage />} />
-        <Route path={PATHS.archive} element={<ArchivePage />} />
-      </Routes>
+      <RouterProvider router={router} />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
-import { BrowserRouter } from 'react-router-dom'
-import ScrollToTop from 'components/atoms/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ScrollToTop>
-        <App />
-      </ScrollToTop>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
