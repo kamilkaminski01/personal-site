82c2cc9ff2d23820463768237b16a66be11a9c84
Kamil Kamiński <kamilkaminski39@gmail.com>
2024-03-29 15:20:28 +0100

    perf: Remove vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,11 @@ import App from './App.tsx'
 import './index.scss'
 import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Analytics />
-      <SpeedInsights />
       <App />
     </BrowserRouter>
   </React.StrictMode>
